252d496b93259d47a811d4a4ca1a48a128fb90b6
Fnab-West <82403442+Fnab-West@users.noreply.github.com>
2022-03-18 20:42:27 -0400

    Update e.js

--- e.js
+++ e.js
@@ -1,3 +1,18 @@
-var x = document.createElement("b");
-x.innerHTML = "hello";
-document.body.appendChild(x);
+var word = "health";
+var length= word.length;
+var space = 20;
+var filler = "▢";
+var rows = 6;
+var marge = 0;
+var list = [];
+var bod = document.createElement("div");
+document.body.appendChild(bod);
+for(var i = 0;i < length;i++){
+  list[i] = [];
+  for(var e = 0;e<rows;e++){
+    list[i][e] = filler;
+  }
+  var text = document.createElement("b");
+  text.innerHTML = list[i].join("");
+  bod.appendChild(text);
+}
