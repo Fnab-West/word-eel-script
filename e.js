53059eed709ffa62a06dabfb289a91d22b9b0449
Fnab-West <82403442+Fnab-West@users.noreply.github.com>
2022-03-19 15:50:42 -0400

    Restored

--- e.js
+++ e.js
@@ -6,21 +6,12 @@ var rows = 6;
 var marge = 0;
 var list = [];
 var bod = document.createElement("div");
-var key;
-var tries = 0;
-function replaceAt(s, i, c) {
-  const arr = [...s];  // Convert string to array
-  arr[i] = c;          // Set char c at pos i
-  return arr.join(''); // Back to string
-}
-
 bod.style.backgroundColor = "#FAF9F6"
 document.body.appendChild(bod);
 bod.style.width= "fit-content";
 var header = document.createElement("h2");
 header.style.fontSize = "100%";
 header.innerHTML = "Word-eel";
-header.style.textAlign = "center";
 bod.appendChild(header);
 header.style.margin = 0;
 var list = document.createElement("ul");
@@ -42,12 +33,3 @@ for(var i = 0;i < length;i++){
   listelements[i] = list;
   marge -= space;
 }
-window.addEventListener("keydown", function(e) {
-    //tested in IE/Chrome/Firefox
-    key = e.keyCode;
-    if(key != 8){
-      const keyName = e.key;
-      var keyString = replaceAt(listelements[1].innerHTML,1,keyName);
-      alert(keyString);
-    }
-  })
